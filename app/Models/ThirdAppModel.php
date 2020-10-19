aa00f63be5b6fc511a5962f0f603ad3013b80ce7
Mrxxm <362190221@qq.com>
2020-10-19 11:26:52 +0800

    提交

---
+++ app/Models/ThirdAppModel.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\Models;
+
+
+class ThirdAppModel
+{
+    protected $table = 'third_app';
+
+    public $timestamps = true;
+
+    protected $dateFormat = 'U';
+
+    const CREATED_AT = 'create_time';
+
+    const UPDATED_AT = 'update_time';
+
+    public static function check(string $username, string $password)
+    {
+        $user = self::where('app_id', '=', $username)
+            ->where('app_secret', '=', md5(md5($password)))
+            ->first();
+
+        return resultToArray($user);
+    }
+}
