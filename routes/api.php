f1e8b2dd0886b84f33d4acebba2228c9d557c48a
Mrxxm <362190221@qq.com>
2020-10-19 11:55:40 +0800

    第三方app登录路由

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ Route::group(['namespace' => 'App\Http\Controllers\Api\v1'], function () {
     Route::any('v1/demo/index','DemoController@index');
     Route::any('v1/login/getToken','LoginController@getToken');
     Route::any('v1/login/verifyToken','LoginController@verifyToken');
+    Route::any('v1/login/getAppToken','LoginController@getAppToken');
 
     Route::group(['middleware' => ['checkToken']], function () {
 
