7f5f53f8df967b8bb9d8ac4edae0b5b09e31b8b1
Mrxxm <362190221@qq.com>
2020-10-19 11:45:05 +0800

    提交

--- config/wx.php
+++ config/wx.php
@@ -2,8 +2,8 @@
 
 
 return [
-    'app_id'           => 'wxd30c4976700557ff',
-    'app_secret'       => '8eb1e7ba91318e572a4e57b86316921a',
+    'app_id'           => 'wx275f9dea54e952ee',
+    'app_secret'       => '88592069faf27f2630e8218c5003b274',
     'login_url'        => "https://api.weixin.qq.com/sns/jscode2session?" . "appid=%s&secret=%s&js_code=%s&grant_type=authorization_code",
     'access_token_url' => "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s"
 ];
