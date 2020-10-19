d2b987b0f0fce4b1a5b00ff67f24b2f34a461a01
Mrxxm <362190221@qq.com>
2020-10-19 11:54:47 +0800

    提交

--- app/Utils/Token.php
+++ app/Utils/Token.php
@@ -12,7 +12,7 @@ class Token
     {
         // 32个字符组成一组随机字符串
         $randChar = getRandChar(32);
-        // 用三组字符串,进行md5加密
+
         $timestamp = $_SERVER['REQUEST_TIME_FLOAT'];
         // salt
         $salt = config('secure.token_salt');
