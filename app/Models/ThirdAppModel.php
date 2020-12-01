88c0234fda6e2bceb38dee7a43335f9a12c3733b
Mrxxm <362190221@qq.com>
2020-12-01 16:39:52 +0800

    提交

--- app/Models/ThirdAppModel.php
+++ app/Models/ThirdAppModel.php
@@ -10,6 +10,10 @@ class ThirdAppModel extends BaseModel
 {
     protected $table = 'third_app';
 
+    const CREATED_AT = 'create_time';
+
+    const UPDATED_AT = 'update_time';
+
     public static function check(string $username, string $password)
     {
         $user = self::where('app_id', '=', $username)
