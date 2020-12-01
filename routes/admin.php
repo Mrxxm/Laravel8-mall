eeeda1b0fdc0ae17691673eb9f08cb56ff8b7889
Mrxxm <362190221@qq.com>
2020-12-01 14:09:59 +0800

    提交

--- routes/admin.php
+++ routes/admin.php
@@ -4,4 +4,19 @@ Route::group(['namespace' => 'App\Http\Controllers\Admin'], function () {
 
     Route::get('/login/getAppToken','LoginController@getAppToken');
 
+    Route::group(['middleware' => ['checkAdminToken']], function () {
+
+        // 后台用户管理
+        Route::get('/adminUser/list','AdminUserController@list');
+        Route::get('/adminUser/add','AdminUserController@add');
+        Route::get('/adminUser/update','AdminUserController@update');
+        Route::get('/adminUser/delete','AdminUserController@delete');
+
+        // 前台用户管理
+        Route::get('/user/list','UserController@list');
+        Route::get('/user/update','UserController@update');
+        Route::get('/user/delete','UserController@delete');
+
+
+    });
 });
