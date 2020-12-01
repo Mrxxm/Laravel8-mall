fa8632645ec612ed2229253e1a6f10059f06273d
Mrxxm <362190221@qq.com>
2020-12-01 13:57:44 +0800

    提交

--- app/Http/Controllers/Api/v1/LoginController.php
+++ app/Http/Controllers/Api/v1/LoginController.php
@@ -66,37 +66,4 @@ class LoginController
         return Response::makeResponse(true, Response::SUCCESS_CODE);
     }
 
-    /**
-     * cms使用
-     * 第三方应用获取令牌
-     * @param Request $request
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function getAppToken(Request $request)
-    {
-        header('Access-Control-Allow-Origin: *');
-        header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-        header('Access-Control-Allow-Methods: GET');
-
-        $data = $request->only('username', 'password');
-
-        $validator = Validator::make($data, [
-            'username'             => 'required|string',
-            'password'             => 'required|string',
-        ]);
-
-        if ($validator->fails()) {
-            return Response::makeResponse(false, Response::MISSING_PARAM, [], $validator->errors()->first());
-        }
-
-        $appTokenService = new AppTokenServiceImpl();
-
-        try {
-            $token = $appTokenService->get($data['username'], $data['password']);
-        } catch (\Exception $exception) {
-            return Response::makeResponse(false, Response::UNKNOWN_ERROR, [], $exception->getMessage());
-        }
-
-        return Response::makeResponse(true, Response::SUCCESS_CODE, ['token' => $token]);
-    }
 }
