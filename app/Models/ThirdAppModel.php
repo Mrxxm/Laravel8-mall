da94ded57ece159d8973504a44e8a0fb57cf3585
Mrxxm <362190221@qq.com>
2020-12-01 16:55:23 +0800

    提交

--- app/Models/ThirdAppModel.php
+++ app/Models/ThirdAppModel.php
@@ -10,6 +10,14 @@ class ThirdAppModel extends BaseModel
 {
     protected $table = 'third_app';
 
+    // 默认true
+    public $timestamps = true;
+
+    // 默认datetime U-时间戳
+    protected $dateFormat = '';
+
+    protected $fillable = ['app_id', 'app_secret', 'app_description', 'scope', 'scope_description'];
+
     const CREATED_AT = 'create_time';
 
     const UPDATED_AT = 'update_time';
