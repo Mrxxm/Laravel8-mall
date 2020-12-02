69bb8ed64a941b1fb90305d62c660d3261e6ce08
Mrxxm <362190221@qq.com>
2020-12-02 09:23:50 +0800

    提交

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -34,7 +34,7 @@ class BaseModel extends Model
 
     public function add(array $fields)
     {
-         return self::create($fields);
+        return self::create($fields);
     }
 
     public function updateById(int $id, array $fields)
