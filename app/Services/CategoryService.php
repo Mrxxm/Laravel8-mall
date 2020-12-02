c7e508ca4d22a8c910b4d7c29084a68c330172c9
Mrxxm <362190221@qq.com>
2020-12-02 17:31:02 +0800

    提交

--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -6,6 +6,8 @@ namespace App\Services;
 
 interface CategoryService
 {
+    public function listAll() : array ;
+
     public function list(array $data) : array ;
 
     public function add(array $fields) : void;
