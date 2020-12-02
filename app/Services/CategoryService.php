89c3799cabd0cb4c76c0889c15c5e01ddcb9b59f
Mrxxm <362190221@qq.com>
2020-12-02 14:58:49 +0800

    提交

---
+++ app/Services/CategoryService.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Services;
+
+
+interface CategoryService
+{
+    public function list(array $data) : array ;
+
+    public function add(array $fields) : void;
+
+    public function update(int $id, array $fields) : void;
+
+    public function delete(int $id) : void;
+}
