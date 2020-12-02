b14bafff5ebaa2403758a32da315a4a5f32fc04f
Mrxxm <362190221@qq.com>
2020-12-02 09:29:53 +0800

    提交

--- app/Services/Impl/UserServiceImpl.php
+++ app/Services/Impl/UserServiceImpl.php
@@ -10,43 +10,40 @@ use Illuminate\Support\Facades\DB;
 
 class UserServiceImpl implements UserService
 {
-    public function getUserById(int $uId): array
+    protected $model = null;
+
+    public function __construct()
+    {
+        $this->model = new UserModel();
+    }
+
+    public function getById(int $uId): array
     {
         $userInfo = UserModel::where('id', '=', $uId)
             ->select('avatar', 'mobile', 'nickname')
             ->first();
         $result = resultToArray($userInfo);
 
-        $addresses = DB::table('user_address')
-            ->where('user_id', '=', $uId)
-            ->get();
-        $address = [];
-        if (count($addresses)) {
-            $address = resultToArray($addresses);
-        }
-
-        $result['address'] = $address;
+//        $addresses = DB::table('user_address')
+//            ->where('user_id', '=', $uId)
+//            ->get();
+//        $address = [];
+//        if (count($addresses)) {
+//            $address = resultToArray($addresses);
+//        }
+//
+//        $result['address'] = $address;
 
         return $result;
     }
 
-    public function updateUserById(int $uId, array $data): void
+    public function updateById(int $uId, array $fields): void
     {
-
-        $field = [];
-        $nickName = $data['nickName'] ?? '';
-        $avatar   = $data['avatarUrl'] ?? '';
-        $mobile   = $data['mobile'] ?? '';
-        if ($nickName)
-            $field['nickname'] = $nickName;
-        if ($avatar)
-            $field['avatar'] = $avatar;
-        if ($mobile)
-            $field['mobile'] = $mobile;
-        if (!empty($field)) {
-            $field['update_time'] = time();
-            UserModel::where('id', '=', $uId)
-                ->update($field);
+        $user = $this->model->find($uId);
+        if (!$user) {
+            throw new \Exception('用户不存在');
         }
+        unset($fields['uId']);
+        $this->model->update($fields);
     }
 }
