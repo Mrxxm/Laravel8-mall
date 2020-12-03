e3d5a9e06d7329f1c7e20e0d681e80c4d33d4beb
Mrxxm <362190221@qq.com>
2020-12-03 19:36:04 +0800

    提交

--- app/Http/Controllers/Admin/GoodsSkuController.php
+++ app/Http/Controllers/Admin/GoodsSkuController.php
@@ -11,29 +11,6 @@ use Illuminate\Support\Facades\Validator;
 
 class GoodsSkuController
 {
-    public function list(Request $request)
-    {
-        $data = $request->only('goods_id');
-
-        $validator = Validator::make($data, [
-            'goods_id'          => 'required|integer',
-        ]);
-
-        if ($validator->fails()) {
-            return Response::makeResponse(false, Response::MISSING_PARAM, [], $validator->errors()->first());
-        }
-
-        $service = new GoodsSkuServiceImpl();
-
-        try {
-            $result = $service->list($data);
-        } catch (\Exception $exception) {
-            return Response::makeResponse(false, Response::UNKNOWN_ERROR, [], $exception->getMessage());
-        }
-
-        return Response::makeResponse(true, Response::SUCCESS_CODE, $result);
-    }
-
     public function update(Request $request)
     {
         $data = $request->only('id', 'stock', 'price', 'cost_price', 'status');
