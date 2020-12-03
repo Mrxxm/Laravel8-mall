2a1d7acfef5253cfc3dea8f5b8416ebcf0a2b4e7
Mrxxm <362190221@qq.com>
2020-12-03 10:28:16 +0800

    提交

---
+++ app/Services/SpecsValueService.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Services;
+
+
+interface SpecsValueService
+{
+    /*
+     * 后台
+     */
+    public function search(array $data) : array;
+
+    public function list(array $data) : array ;
+
+    public function add(array $fields) : void;
+
+    public function update(int $id, array $fields) : void;
+
+    public function delete(int $id) : void;
+}
