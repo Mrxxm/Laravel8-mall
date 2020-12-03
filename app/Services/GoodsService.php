5a1becfe4aa1044275a4957140d8c3149575d0f6
Mrxxm <362190221@qq.com>
2020-12-03 11:27:30 +0800

    提交

---
+++ app/Services/GoodsService.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Services;
+
+
+interface GoodsService
+{
+    /*
+     * 后台
+     */
+    public function list(array $data) : array ;
+
+    public function add(array $fields) : void;
+
+    public function update(int $id, array $fields) : void;
+
+    public function delete(int $id) : void;
+}
