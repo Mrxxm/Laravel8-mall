7c81c3009f8ca4643a92c59e85f2d7385a86fa3a
Mrxxm <362190221@qq.com>
2020-12-03 09:54:11 +0800

    提交

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -6,9 +6,15 @@ namespace App\Services;
 
 interface UserService
 {
+    /*
+     * 前台
+     */
     public function getById(int $uId) : array ;
 
     public function updateById(int $uId, array $fields) : void;
 
+    /*
+     * 后台
+     */
     public function list(array $data) : array ;
 }
