ebd8f944a77c0447bebd3218287d92aafd405075
Mrxxm <362190221@qq.com>
2020-12-03 10:02:02 +0800

    提交

---
+++ app/Http/Controllers/Admin/SpecsController.php
@@ -0,0 +1,129 @@
+<?php
+
+
+namespace App\Http\Controllers\Admin;
+
+
+use App\Http\Controllers\Api\Response;
+use App\Services\Impl\SpecsServiceImpl;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class SpecsController
+{
+    public function search(Request $request)
+    {
+        $data = $request->only('keyword');
+
+        $validator = Validator::make($data, [
+            'keyword'              => 'string',
+        ]);
+
+        if ($validator->fails()) {
+            return Response::makeResponse(false, Response::MISSING_PARAM, [], $validator->errors()->first());
+        }
+
+        $service = new SpecsServiceImpl();
+
+        try {
+            $result = $service->search($data);
+        } catch (\Exception $exception) {
+            return Response::makeResponse(false, Response::UNKNOWN_ERROR, [], $exception->getMessage());
+        }
+
+        return Response::makeResponse(true, Response::SUCCESS_CODE, $result);
+    }
+
+    public function list(Request $request)
+    {
+        $data = $request->only('keyword');
+
+        $validator = Validator::make($data, [
+            'keyword'          => 'string',
+        ]);
+
+        if ($validator->fails()) {
+            return Response::makeResponse(false, Response::MISSING_PARAM, [], $validator->errors()->first());
+        }
+
+        $service = new SpecsServiceImpl();
+
+        try {
+            $result = $service->list($data);
+        } catch (\Exception $exception) {
+            return Response::makeResponse(false, Response::UNKNOWN_ERROR, [], $exception->getMessage());
+        }
+
+        return Response::makeResponse(true, Response::SUCCESS_CODE, $result);
+    }
+
+    public function add(Request $request)
+    {
+        $data = $request->only('name');
+
+        $validator = Validator::make($data, [
+            'name'             => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return Response::makeResponse(false, Response::MISSING_PARAM, [], $validator->errors()->first());
+        }
+
+        $service = new SpecsServiceImpl();
+
+        try {
+            $service->add($data);
+        } catch (\Exception $exception) {
+            return Response::makeResponse(false, Response::UNKNOWN_ERROR, [], $exception->getMessage());
+        }
+
+        return Response::makeResponse(true, Response::SUCCESS_CODE);
+    }
+
+    public function update(Request $request)
+    {
+        $data = $request->only('id', 'status');
+
+        $validator = Validator::make($data, [
+            'id'               => 'required|integer',
+            'status'           => 'integer',
+        ]);
+
+        if ($validator->fails()) {
+            return Response::makeResponse(false, Response::MISSING_PARAM, [], $validator->errors()->first());
+        }
+
+        $service = new SpecsServiceImpl();
+
+        try {
+            $service->update($data['id'], $data);
+        } catch (\Exception $exception) {
+            return Response::makeResponse(false, Response::UNKNOWN_ERROR, [], $exception->getMessage());
+        }
+
+        return Response::makeResponse(true, Response::SUCCESS_CODE);
+    }
+
+    public function delete(Request $request)
+    {
+        $data = $request->only('id');
+
+        $validator = Validator::make($data, [
+            'id'             => 'required|integer',
+        ]);
+
+        if ($validator->fails()) {
+            return Response::makeResponse(false, Response::MISSING_PARAM, [], $validator->errors()->first());
+        }
+
+        $service = new SpecsServiceImpl();
+
+        try {
+            $service->delete($data['id']);
+        } catch (\Exception $exception) {
+            return Response::makeResponse(false, Response::UNKNOWN_ERROR, [], $exception->getMessage());
+        }
+
+        return Response::makeResponse(true, Response::SUCCESS_CODE);
+    }
+}
