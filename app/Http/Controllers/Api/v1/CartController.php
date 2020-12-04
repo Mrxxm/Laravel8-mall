ee5296b9e760bba25821fd12e990e13cb48c4e84
Mrxxm <362190221@qq.com>
2020-12-04 15:21:13 +0800

    提交

--- app/Http/Controllers/Api/v1/CartController.php
+++ app/Http/Controllers/Api/v1/CartController.php
@@ -35,12 +35,13 @@ class CartController
         return Response::makeResponse(true, Response::SUCCESS_CODE);
     }
 
-    public function delete(Request $request)
+    public function update(Request $request)
     {
-        $data = $request->only('ids');
+        $data = $request->only('sku_id', 'num');
 
         $validator = Validator::make($data, [
-            'ids'              => 'required|string',
+            'sku_id'              => 'required|integer',
+            'num'                 => 'required|integer',
         ]);
 
         if ($validator->fails()) {
@@ -50,7 +51,30 @@ class CartController
         $service = new CartServiceImpl();
 
         try {
-            $service->delete($data['ids']);
+            $service->update($data['sku_id'], $data);
+        } catch (\Exception $exception) {
+            return Response::makeResponse(false, Response::UNKNOWN_ERROR, [], $exception->getMessage());
+        }
+
+        return Response::makeResponse(true, Response::SUCCESS_CODE);
+    }
+
+    public function delete(Request $request)
+    {
+        $data = $request->only('sku_ids');
+
+        $validator = Validator::make($data, [
+            'sku_ids'              => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return Response::makeResponse(false, Response::MISSING_PARAM, [], $validator->errors()->first());
+        }
+
+        $service = new CartServiceImpl();
+
+        try {
+            $service->delete($data['sku_ids']);
         } catch (\Exception $exception) {
             return Response::makeResponse(false, Response::UNKNOWN_ERROR, [], $exception->getMessage());
         }
