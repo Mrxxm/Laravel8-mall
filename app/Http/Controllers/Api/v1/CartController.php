7cf877d7056a1ed96e53e92c3714d9ccf659a5c4
Mrxxm <362190221@qq.com>
2020-12-04 14:54:00 +0800

    提交

---
+++ app/Http/Controllers/Api/v1/CartController.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace App\Http\Controllers\Api\v1;
+
+
+use App\Http\Controllers\Api\Response;
+use App\Services\Impl\CartServiceImpl;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class CartController
+{
+    public function add(Request $request)
+    {
+        $data = $request->only('sku_id', 'num');
+
+        $validator = Validator::make($data, [
+            'sku_id'           => 'required|integer',
+            'num'              => 'required|integer',
+        ]);
+
+        if ($validator->fails()) {
+            return Response::makeResponse(false, Response::MISSING_PARAM, [], $validator->errors()->first());
+        }
+
+        $service = new CartServiceImpl();
+
+        try {
+            $service->add($data);
+        } catch (\Exception $exception) {
+            return Response::makeResponse(false, Response::UNKNOWN_ERROR, [], $exception->getMessage());
+        }
+
+        return Response::makeResponse(true, Response::SUCCESS_CODE);
+    }
+}
