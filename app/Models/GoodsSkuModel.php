84081364e7c587977de0aae7f743387dc0500afa
Mrxxm <362190221@qq.com>
2020-12-04 10:16:51 +0800

    提交

--- app/Models/GoodsSkuModel.php
+++ app/Models/GoodsSkuModel.php
@@ -17,7 +17,18 @@ class GoodsSkuModel extends BaseModel
 
     public function goods()
     {
+        /**
+         * hasOne
+            1、外键保存在关联表中;
+            2、保存时自动更新关联表的记录;
+            3、删除主表记录时自动删除关联记录;
+           belongsTo
+            1、外键放置在主表中;
+            2、保存时不会自动更新关联表的记录;
+            3、删除时也不会更新关联表的记录;
+         */
         return $this->hasOne('App\Models\GoodsModel', 'id', 'goods_id');
+//        return $this->belongsTo('App\Models\GoodsModel', 'goods_id', 'id');
     }
 
     public function updateByGoodsId(int $goodsId, array $fields)
