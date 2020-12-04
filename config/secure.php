a0af4445b8f831831d5286d4f3e3da178a5e203b
Mrxxm <362190221@qq.com>
2020-12-04 16:23:38 +0800

    提交

--- config/secure.php
+++ config/secure.php
@@ -4,5 +4,5 @@
 return [
     'token_salt' => 'HHaafhGAD39ERe3f',
     'pay_back_url' => 'https://www.kenrou.cn/api/v1/pay/notify', // 本地虚拟域名无法访问
-    'token_expire_in' => 7200,
+    'token_expire_in' => 24 * 60 * 60,
 ];
