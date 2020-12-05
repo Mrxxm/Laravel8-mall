7ca6ece2c46dcf079d03116ae40fa2192f53e9d6
Mrxxm <362190221@qq.com>
2020-12-05 17:14:24 +0800

    提交

--- app/Models/OrderGoodsModel.php
+++ app/Models/OrderGoodsModel.php
@@ -9,7 +9,7 @@ class OrderGoodsModel extends BaseModel
     protected $table = 'order_goods';
 
     // 使用create方法添加时，需判断
-    protected $fillable = ['order_no', 'sku_id', 'goods_id', 'num', 'title', 'price', 'total_price', 'image'];
+    protected $fillable = ['order_no', 'sku_id', 'goods_id', 'num', 'title', 'sku', 'price', 'total_price', 'image'];
 
     const CREATED_AT = 'create_time';
 
