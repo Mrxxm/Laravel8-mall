85d2f873fe5c68237f8d2e447de12bfdfa9e9522
Mrxxm <362190221@qq.com>
2020-12-05 12:06:11 +0800

    提交

---
+++ app/Http/Controllers/Api/v1/OrderController.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace App\Http\Controllers\Api\v1;
+
+
+use App\Http\Controllers\Api\Response;
+use App\Services\Impl\OrderServiceImpl;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class OrderController
+{
+    public function add(Request $request)
+    {
+        $data = $request->only('address_id', 'sku_ids');
+
+        $validator = Validator::make($data, [
+            'address_id'         => 'required|integer',
+            'sku_ids'            => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return Response::makeResponse(false, Response::MISSING_PARAM, [], $validator->errors()->first());
+        }
+
+        $service = new OrderServiceImpl();
+
+        try {
+            $service->add($data);
+        } catch (\Exception $exception) {
+            return Response::makeResponse(false, Response::UNKNOWN_ERROR, [], $exception->getMessage());
+        }
+
+        return Response::makeResponse(true, Response::SUCCESS_CODE);
+    }
+}
