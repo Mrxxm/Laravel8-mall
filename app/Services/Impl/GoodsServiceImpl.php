cc73a27f5e4bede592baa638c5f91230e26b4d5b
Mrxxm <362190221@qq.com>
2020-12-05 13:58:55 +0800

    提交

--- app/Services/Impl/GoodsServiceImpl.php
+++ app/Services/Impl/GoodsServiceImpl.php
@@ -143,53 +143,25 @@ class GoodsServiceImpl implements GoodsService
             throw new \Exception('商品已删除');
         }
         $skuId     = $goods->sku_id;
-        $specsType = $goods->goods_specs_type;
         $goodsId   = $goods->id;
-        if ($specsType == 1) {
-            // 统一规格
-            $goodsSku = $this->goodsSkuService->model->find($skuId);
-            if (!$goodsSku) {
-                throw new \Exception('商品sku不存在');
-            }
-            if ($goodsSku->delete_time != 0) {
-                throw new \Exception('商品sku已删除');
-            }
-            // 更新商品表
-            $this->model->updateById($id, $fields);
-            $goodsSkuUpd = [];
-            if (isset($fields['price'])) {
-                $goodsSkuUpd['price'] = $fields['price'];
-            }
-            if (isset($fields['cost_price'])) {
-                $goodsSkuUpd['cost_price'] = $fields['cost_price'];
-            }
-            if (isset($fields['stock'])) {
-                $goodsSkuUpd['stock'] = $fields['stock'];
-            }
-            if (isset($fields['status'])) {
-                $goodsSkuUpd['status'] = $fields['status'];
-            }
-            if (!empty($goodsSkuUpd)) {
-                // 更新商品sku表
-                $this->goodsSkuService->model->updateById($skuId, $goodsSkuUpd);
-            }
 
-        } else {
-            // 多规格
-            // 防止修改这三个参数
-            unset($fields['price']);
-            unset($fields['cost_price']);
-            unset($fields['stock']);
-            // 更新商品表
-            $this->model->updateById($id, $fields);
-            $goodsSkuUpd = [];
-            if (isset($fields['status'])) {
-                $goodsSkuUpd['status'] = $fields['status'];
-            }
-            if (!empty($goodsSkuUpd)) {
-                // 更新商品sku表
-                $this->goodsSkuService->model->updateByGoodsId($goodsId, $goodsSkuUpd);
-            }
+        $goodsSku = $this->goodsSkuService->model->find($skuId);
+        if (!$goodsSku) {
+            throw new \Exception('商品sku不存在');
+        }
+        if ($goodsSku->delete_time != 0) {
+            throw new \Exception('商品sku已删除');
+        }
+
+        // 更新商品表
+        $this->model->updateById($id, $fields);
+        $goodsSkuUpd = [];
+        if (isset($fields['status'])) {
+            $goodsSkuUpd['status'] = $fields['status'];
+        }
+        if (!empty($goodsSkuUpd)) {
+            // 更新商品sku表(包含多规格更新)
+            $this->goodsSkuService->model->updateByGoodsId($goodsId, $goodsSkuUpd);
         }
 
         return ;
