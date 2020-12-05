a7bf3c23eb12a01ba6b82f3ee124b5744716a26b
Mrxxm <362190221@qq.com>
2020-12-05 11:03:22 +0800

    提交

--- app/Http/Controllers/Api/v1/DemoController.php
+++ app/Http/Controllers/Api/v1/DemoController.php
@@ -4,11 +4,20 @@
 namespace App\Http\Controllers\Api\v1;
 
 
+use App\Utils\Snowflake;
+
 class DemoController
 {
     public function index()
     {
         $randChar = getRandChar(32);
         return $randChar;
+    }
+
+    public function snowflake()
+    {
+        // workId 1 ~ 1023
+        $NO = Snowflake::getInstance()->setWorkId(1)->id();
+        return $NO;
     }
 }
