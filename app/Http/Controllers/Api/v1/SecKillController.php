6215dbe5043abf9816daafa9052a99c8d0f093df
Mrxxm <362190221@qq.com>
2020-12-17 14:51:33 +0800

    提交

--- app/Http/Controllers/Api/v1/SecKillController.php
+++ app/Http/Controllers/Api/v1/SecKillController.php
@@ -34,7 +34,7 @@ class SecKillController
     // 非阻塞 不公平
     public function sharedLock(Request $request)
     {
-        $storage = DB::table('storage')
+        $storage = DB::table('b_storage')
             ->where('id', '=', 1)
             ->first();
 
@@ -43,7 +43,7 @@ class SecKillController
         $upd = [];
         $upd['number'] = $quantity - self::COUNT;
 
-        $result = DB::table('storage')
+        $result = DB::table('b_storage')
             ->where('id', '=', 1)
             ->where('number', '>=', self::COUNT)
             ->update($upd);
@@ -51,7 +51,7 @@ class SecKillController
         if ($result) {
             $insert = [];
             $insert['number'] = $quantity;
-            DB::table('order')
+            DB::table('b_order')
                 ->insert($insert);
         }
 
