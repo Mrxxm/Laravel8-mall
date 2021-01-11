71ba192d2519e230a506b1322564dca5e00a5355
Mrxxm <362190221@qq.com>
2021-01-11 16:09:29 +0800

    门面

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use App\Services\Impl\UserServiceImpl;
+use App\Services\Impl\AdminUserServiceImpl;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,9 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-//        $this->app->singleton('App\Services\UserService', function ($app){
-//            return new UserServiceImpl();
-//        });
+        $this->app->singleton('AdminUS', function ($app){
+            return new AdminUserServiceImpl();
+        });
     }
 
     /**
